60303a64039a2a37909bb8ade0380d4e976598e8
hjonasson <hjorturjonasson@gmail.com>
2017-02-03 15:21:53 +0100

    added pseudonyms

--- pyboilerplate.py
+++ pyboilerplate.py
@@ -6,45 +6,62 @@ Parameters
 '''
 
 filename , args = sys.argv[ 1 ] , sys.argv[ 2: ]
-keysImports = { 'plt' : 'import seaborn as sns\nimport matplotlib.pylab as plt ' ,
-				'pd' : 'import pandas as pd' ,
-				'glob' : 'from glob import glob' ,
-				'np' : 'import numpy as np'
+keysImports = { 'plt' : 'seaborn as sns\nimport matplotlib.pylab as plt ' ,
+				'pd' : 'pandas as pd' ,
+				'np' : 'numpy as np'
 				}
 
 '''
 Functions
 '''
 
-def reqImports():
+# flattening a list of lists
+flatten = lambda l: [ item for sublist in l for item in sublist ]
 
-	# filter the keys that are both in args and keysImports
-	return { i : keysImports[ i ] for i in filter( lambda j : j in args , keysImports ) }
+# make values into an import string
+stringer = lambda l : 'import ' + ' '.join( map( lambda _: '%s' , l[ 0 ] if isinstance( l[ 0 ] , tuple ) else [ None ] ) ) + '\n'
 
 def parts( l = [ 'Parameters' , 'Functions' , 'Execution' ] ):
 
+	# define the string that sections off parts of the code
 	return "\n\n'''\n" + "\n'''\n\n\n\n'''\n".join( l ) + "\n'''\n\n"
+
+def writer( f , l ):
+
+	# make string needed for formatting
+	st = stringer( l )
+	for i in l:
+
+		# write to the file with string formatting
+		f.write( st % i )
+
+	# file return
+	return f
 
 '''
 Execution
 '''
 
+# filter the keys that are both in args and keysImports
+reqImports = [ keysImports[ i ] for i in filter( lambda j : j in args , keysImports ) ]
+
+# get indices of 'as' in requested to find all values that should have a pseudonym
+reqPseuInds = map( lambda i : i[ 0 ] , filter( lambda ( i , j ) : j == 'as' , enumerate( args ) ) )
+
+# get modules and pseudonyms
+modPseu = map( lambda i : tuple( args[ i - 1 : i + 2 ] ) , reqPseuInds )
+
+# filter requested imports that are not predefined
+rINP = filter( lambda a : a not in reqImports + flatten( modPseu ) , args )
+
 # create file with the requested filename
 f = open( filename.endswith( '.py' ) and filename or '%s.py' % filename , 'w' )
 
-# filter the requested imports and how to import them
-rI = reqImports()
+# iterate over all imports to add
+for reqs in [ reqImports , rINP , modPseu ]:
 
-# write the requested imports
-for i in rI: 
-	f.write( '%s\n' % rI[ i ] )
-
-# filter requested imports that are not predefined
-rINP = filter( lambda a : a not in rI.keys() , args )
-
-# write those imports
-for i in rINP:
-	f.write( 'import %s\n' % i )
+	# call writer function defined above
+	f = writer( f , reqs )
 
 # write the areas for functionality
 f.write( parts() )
